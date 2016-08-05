97358a1fd952fa4ada77f271c5ede5037b7ae321
Nirakar Mohanty <nirakarmohanty@gmail.com>
2016-08-05 20:44:41 +0530

    Added java doc

--- src/main/java/com/nirakar/spring/jdbc/dao/customer/CustomerDAO.java
+++ src/main/java/com/nirakar/spring/jdbc/dao/customer/CustomerDAO.java
@@ -1,5 +1,10 @@
-package com.nirakar.springjdbc.dao.customer;
+package com.nirakar.spring.jdbc.dao.customer;
 
+/**
+ * Defines different methods to do Data base operation.
+ * @author NIRAKAR
+ *
+ */
 public interface CustomerDAO {
 
 	public int insert(Customer cust);
